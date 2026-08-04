6f7e8db9ed2c23261b4edccffa8e4051688a9259
owairkhan369 <MuhammadOwaiskhanBuneri0346@email.com>
2026-08-04 23:11:22 +0500

    first class

---
+++ class1.py
@@ -0,0 +1,37 @@
+print("Hello Word")
+print('hello word2')
+print("abcdefghijklmnopqrstuvwxyz")
+print("haq public school")
+print("ayankhan")
+print("muhammadowaiskhan")
+print('laptop')
+print("computer")
+print('keyboard')
+print("screen")
+
+name = "owais"
+print(type(name))
+numbers = 123456789
+print(type(numbers))
+alphabets = "abcdefghijklmnopqrstuvwxyz"
+print(type(alphabets))
+number = 15
+print(type(numbers))
+symbols = "@$#%^7*+-/"
+print(type(symbols))
+country = "Pakistan"
+print(type(country))
+numbers = 123456
+print(type(numbers))
+counting = 123456789101112131415161718192021222324252627282930
+print(type(counting))
+number = 100000
+print(type(number))
+fruitname = "apple"
+print(type(fruitname))
+vegatablename = "tomato"
+print(type("vegetablename"))
+animalname = "markhor"
+print(type("animalname"))
+
+
